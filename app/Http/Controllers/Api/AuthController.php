b33b0c9acc727ce84fae83bf81680e2ea9ef2658
cereceda1991 <cereceda1991@gmail.com>
2023-07-05 06:49:14 -0500

    feat : add input validations

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,31 +5,51 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-    
-        if (!Auth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        // Validar los datos de entrada
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
         }
-    
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!Auth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Error creating token'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
         $user = Auth::user();
-        $user->makeHidden('password'); // Ocultar la contraseña en la respuesta
+        $user->makeHidden('password'); 
         $token = JWTAuth::fromUser($user);
-    
+
         return response()->json(['user' => $user, 'token' => $token], Response::HTTP_OK);
     }
-    
+
     public function logout(Request $request)
     {
-        // Revocar el token de acceso del usuario actual
-        JWTAuth::invalidate(JWTAuth::getToken());
+        try {
+            // Revocar el token de acceso del usuario actual
+            JWTAuth::invalidate(JWTAuth::getToken());
 
-        return response()->json(['message' => 'Successfully logged out'], Response::HTTP_OK);
+            return response()->json(['message' => 'Logout successful'], Response::HTTP_OK);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Failed to sign out'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
